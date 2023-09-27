e479bb2458ddeb408d7f839447df45d6ce14c14c
Daniel Genkins <64920363+d-genk@users.noreply.github.com>
2023-09-27 16:57:05 -0500

    combined transformation and concatenation fxns and made nb

---
+++ transform_and_combine.py
@@ -0,0 +1,29 @@
+import os
+
+def combine_jsonl(path_to_volume_root):
+    for folder, subfolders, files in os.walk(path_to_volume_root):
+        official_text = ""
+        for file in files:
+            if ".DS" in file:
+                continue     
+            out_path = file[:file.find(".")] + ".jsonl"           
+
+            with open(file, "r") as infile:
+                for line in infile:
+                    while line[0]== " ":
+                        line = line [1:]
+                    if line[0] == "<":
+                        continue
+                    line = line.replace("\n", "")
+                    line = line.replace("\"", "")
+                    if len(line) == 0:
+                        continue      
+                            
+                    linea= "{\"text\": \"" + line + "\"}\n"
+                    official_text += linea            
+    
+    path_components = path_to_volume_root.split("\\")
+    id = path_components[len(path_components) - 1]
+    
+    with open(os.path.join(path_to_volume_root, id + ".jsonl"), "w", encoding="utf-8") as outfile:
+        outfile.write(official_text)
